530833ad4c31b6bc1c7139b35fb51322866261c5
bchang0999 <bchang0999@gmail.com>
2022-05-04 14:56:43 -0400

    chaining methods

--- class/practice/user.py
+++ class/practice/user.py
@@ -7,25 +7,28 @@ class User:
 
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
 
     def make_withdrawl(self, amount):
         self.account_balance -= amount
+        return self
 
     def display_user_balance(self):
         print (self.name)
         print (self.account_balance)
+        return self
 
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
+        return self
 
 
 guido = User("Guido van Rossum", "guido@python.com")
 monty = User('Monty Python', 'monty@python.com')
 
 
-guido.make_deposit(1000)
-guido .transfer_money(monty, 500)
+guido.make_deposit(1000).transfer_money(monty, 500)
 monty.display_user_balance()
 
 
